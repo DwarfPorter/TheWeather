4199d91b7379c8f6b79d662b96dca0db2f8f0dd1
vladimir.morozov <vladimir.morozov@totalobjects.spb.ru>
2018-11-07 11:21:09 +0300

    Добавил список городов

--- app/src/main/java/ru/geekbrains/theweather/ChooserCityActivity.java
+++ app/src/main/java/ru/geekbrains/theweather/ChooserCityActivity.java
@@ -1,7 +1,10 @@
 package ru.geekbrains.theweather;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ChooserCityActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class ChooserCityActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chooser_city);
+
+        setClickListenersOnText(R.id.txtMsk);
+        setClickListenersOnText(R.id.txtSpb);
+        setClickListenersOnText(R.id.txtNsb);
     }
+
+    private void setClickListenersOnText(int id){
+        TextView txt = findViewById(id);
+        txt.setOnClickListener(clickListener);
+    }
+
+    View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String city = ((TextView) v).getText().toString();
+            Intent intentResult = new Intent();
+            intentResult.putExtra("RESULT", city);
+            setResult(RESULT_OK, intentResult);
+            finish();
+        }
+    };
 }
--- app/src/main/java/ru/geekbrains/theweather/MainActivity.java
+++ app/src/main/java/ru/geekbrains/theweather/MainActivity.java
@@ -1,13 +1,27 @@
 package ru.geekbrains.theweather;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int ChooseActivity = 869;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button chooserCity = findViewById(R.id.buttonCity);
+        chooserCity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentChooseCity = new Intent(getApplicationContext(), ChooserCityActivity.class);
+                startActivityForResult(intentChooseCity, ChooseActivity);
+            }
+        });
     }
 }
