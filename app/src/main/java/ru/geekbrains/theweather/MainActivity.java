488e18af36a78bb5ba14f28472aba27c016d2874
vladimir.morozov <vladimir.morozov@totalobjects.spb.ru>
2018-11-07 15:02:34 +0300

    Добавил обработка результата

--- app/src/main/java/ru/geekbrains/theweather/MainActivity.java
+++ app/src/main/java/ru/geekbrains/theweather/MainActivity.java
@@ -1,10 +1,12 @@
 package ru.geekbrains.theweather;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,5 +25,15 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intentChooseCity, ChooseActivity);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode != ChooseActivity) return;
+        if (resultCode != RESULT_OK) return;
+        if (data == null) return;
+
+        TextView txtCity = findViewById(R.id.textCity);
+        txtCity.setText(data.getStringExtra("RESULT"));
     }
 }
